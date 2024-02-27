7d20a5277b9d0d33fa6d8c5b5457f3a1cd9ed43f
Mohsin Khan <mkhan6720@gmail.com>
2024-02-27 19:20:06 +0530

    removed navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,6 @@ export default function Home() {
       <ImageGallery />
       <Projects title="projects" isInProgress={false} />
       <Projects title="upcoming" isInProgress={true} />
-      <About />
     </section>
   );
 }
@@ -93,12 +92,12 @@ function Projects({ title, isInProgress }) {
           ))}
         </section>
       </section>
-      <section className="mdlg:gap-16 flex w-full flex-col gap-10 xs:gap-12 lg:gap-20">
+      <section className="flex w-full flex-col gap-10 xs:gap-12 mdlg:gap-16 lg:gap-20">
         {projects.map((project, i) => {
           if (project.inProgress === isInProgress) {
             return (
               <section
-                className="mdlg:gap-16 flex flex-col gap-10 xs:gap-12 lg:gap-20"
+                className="flex flex-col gap-10 xs:gap-12 mdlg:gap-16 lg:gap-20"
                 key={project.id}
               >
                 <Project
@@ -124,7 +123,7 @@ function Projects({ title, isInProgress }) {
 
 function Project({ name, description, overview, url, techStack, thumbnail }) {
   return (
-    <section className="mdlg:flex-row flex flex-col gap-8 lg:gap-10 xl:gap-12">
+    <section className="flex flex-col gap-8 mdlg:flex-row lg:gap-10 xl:gap-12">
       <img
         className="rounded-xl sm:max-w-xs lg:max-w-sm"
         src={thumbnail}
@@ -164,68 +163,5 @@ function TechStack({ tech }) {
 function HLine() {
   return (
     <section className="border-b border-grey8 dark:border-grey3"></section>
-  );
-}
-
-function About() {
-  const navigate = useNavigate();
-
-  const handleAboutBtn = () => {
-    navigate("/about");
-  };
-
-  return (
-    <section className="flex gap-8 px-6 py-24 md:px-10 xl:px-20">
-      <section
-        onClick={handleAboutBtn}
-        className="cursor-pointer xs:mr-4 sm:mr-6 xl:mr-14"
-      >
-        <section className="sticky top-4">
-          {[..."about"].map((word, i) => (
-            <section
-              key={i}
-              className="flex flex-col items-center text-4xl uppercase text-grey7 dark:text-grey3 xs:gap-2 xs:text-5xl sm:text-6xl xl:text-7xl"
-            >
-              <span>{word}</span>
-            </section>
-          ))}
-        </section>
-      </section>
-
-      <section className="items-top flex flex-col gap-6 sm:gap-10 md:gap-12 lg:flex-row xl:gap-24">
-        <div>
-          <img
-            src="mohsin.png"
-            alt="mohsin khan"
-            className="mb-14 sm:max-w-xs lg:max-w-sm"
-          ></img>
-          <SocialIcons />
-        </div>
-        <article className="flex w-full flex-col justify-start gap-6">
-          <h1 className="text-2xl font-semibold text-grey1 transition-colors dark:text-grey8 sm:text-4xl sm:leading-[50px] lg:leading-[50px] xl:text-5xl xl:leading-[62px]">
-            Harnessing the Potential of Blockchain Technology
-          </h1>
-          <p>
-            Hey there, I'm Mohsin Khan, a Blockchain Developer hailing from
-            India and passionate about crafting software solutions using
-            blockchain technology with great user experiences.
-          </p>
-          <p>
-            My journey began with a curiosity-driven exploration of
-            cryptocurrencies and their underlying blockchain technology. As I
-            delved deeper, I realized the immense potential for blockchain
-            beyond just digital currencies.
-          </p>
-          <p>
-            Driven by a desire to contribute to this rapidly evolving field,
-            I've honed my skills in blockchain development, smart contract
-            programming, and decentralized application (DApp) creation.
-          </p>
-          {/* <p className="cursor-pointer text-grey8" onClick={handleAboutBtn}>
-            More...
-          </p> */}
-        </article>
-      </section>
-    </section>
   );
 }
